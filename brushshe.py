1fbeb76891795ba75469d138b0a21aebca963c72
l1mafresh <omdjeyy@gmail.com>
2024-07-15 09:20:31 +0300

    Вийшла версія v04
    - "Моя галерея" - безпечне місце, де зберігаються малюнки, створені в Брашше;
    - Нові наліпки.

--- brushshe.py
+++ brushshe.py
@@ -6,6 +6,8 @@ from CTkMessagebox import *
 from PIL import Image, ImageDraw, ImageTk, ImageGrab, ImageFont
 from tkinter import EventType, PhotoImage
 import gc
+import os
+import uuid
 
 class Brushshe(CTk):
     def __init__(self):
@@ -59,13 +61,20 @@ class Brushshe(CTk):
             "Квітка": Image.open("stickers/flower.png"),
             "Серце": Image.open("stickers/heart.png"),
             "ОКей": Image.open("stickers/okay.png"),
-            "Сир": Image.open("stickers/cheese.png")
+            "Сир": Image.open("stickers/cheese.png"),
+            "Трава": Image.open("stickers/grass.png"),
+            "Дощ": Image.open("stickers/rain.png"),
+            "Полуниця": Image.open("stickers/strawberry.png"),
+            "Метелик": Image.open("stickers/butterfly.png"),
+            "Квітка2": Image.open("stickers/flower2.png")
         }
 
         add_text_menu = menu.add_cascade("Текст")
         dropdown4 = CustomDropdownMenu(widget=add_text_menu)
         dropdown4.add_option(option="Додати текст на малюнок", command=self.add_text_window_show)
         dropdown4.add_option(option="Змінити розмір", command=self.change_text_size_show)
+
+        my_gallery_menu = menu.add_cascade("Моя галерея", command=self.show_gallery_window)
 
         about_menu = menu.add_cascade("Про Brushshe", command=self.about_program)
 
@@ -86,6 +95,9 @@ class Brushshe(CTk):
 
         self.size_slider_label = CTkLabel(tools_frame, text="2")
         self.size_slider_label.pack(side=LEFT, padx=1)
+
+        save_in_gallery_button = CTkButton(tools_frame, text="Зберегти в галерею", command=self.save_in_gallery)
+        save_in_gallery_button.pack(side=RIGHT, padx=5)
 
 # Канва
         self.canvas = CTkCanvas(self, bg="white")
@@ -140,7 +152,7 @@ class Brushshe(CTk):
         if response=="Ні":
             app.destroy()
         elif response== "Зберегти":
-            self.save_img()
+            self.save_in_gallery()
         else:
             pass
 
@@ -203,7 +215,8 @@ class Brushshe(CTk):
             self.canvas.configure(bg=bg_getcolor)
         
     def show_sticker_choose(self):
-        sticker_choose = CTkToplevel(app, width=300, height=420)
+        sticker_choose = CTkToplevel(app)
+        sticker_choose.geometry("300x420")
         sticker_choose.title("Оберіть наліпку")
 
         # Segmented Button
@@ -220,7 +233,7 @@ class Brushshe(CTk):
                     sticker_btn = CTkButton(stickers_frame, text=None, image=image,
                                             command=lambda img=image: self.set_current_sticker(img))
                     sticker_btn.grid(row=row, column=column, padx=10, pady=10)
-                    column +=1
+                    column += 1
                     if column == 2:
                         column = 0
                         row +=1
@@ -288,10 +301,46 @@ class Brushshe(CTk):
     def change_text_size(self, size):
         self.font_size = int(size)
         self.tx_size_label.configure(text=self.font_size)
+
+    def show_gallery_window(self):
+        my_gallery = CTkToplevel(app)
+        my_gallery.title("Галерея Brushshe")
+        my_gallery.geometry("650x580")
+
+        title_label = CTkLabel(my_gallery, text="Моя галерея")
+        title_label.pack()
+
+        gallery_frame = CTkScrollableFrame(my_gallery, width=650, height=560)
+        gallery_frame.pack()
+
+        row = 0
+        column = 0
+
+        def open_from_gallery(img_path):
+            image = Image.open(img_path)
+            self.image = image
+            self.draw = ImageDraw.Draw(self.image)
+            self.canvas.delete("all")
+            self.photo = ImageTk.PhotoImage(self.image)
+            self.canvas.create_image(0, 0, anchor=NW, image=self.photo)
+                
+        for filename in os.listdir("gallery"):
+            if filename.endswith(".png"):
+                img_path = os.path.join("gallery", filename)
+                img = Image.open(img_path)
+
+                button_image = CTkImage(img, size=(250, 250))
+                image_button = CTkButton(gallery_frame, image=button_image, text=None, command=lambda img_path=img_path: open_from_gallery(img_path))
+                image_button.grid(row=row, column=column, padx=10, pady=10)
+                
+                column += 1
+                if column == 2:
+                    column = 0
+                    row +=1
         
     def about_program(self):
         about_msg = CTkMessagebox(title="Про програму",
-                                  message="Brushshe (Брашше) - програма для малювання, в якій можна створювати те, що Вам подобається.\n\nОрел на ім'я Brucklin (Браклін) - її талісман.\n\nhttps://github.com/l1mafresh/Brushshe\n\nv0.3.2",
+                                  message="Brushshe (Брашше) - програма для малювання, в якій можна створювати те, що Вам подобається.\n\nОрел на ім'я Brucklin (Браклін) - її талісман.\n\nhttps://github.com/l1mafresh/Brushshe\n\nv0.4",
                                   icon="icons/brucklin.png", icon_size=(150,191), option_1="Зрозуміло", height=400)
 
     def clean_all(self):
@@ -300,6 +349,24 @@ class Brushshe(CTk):
     def change_brush_size(self, size):
         self.brush_size = int(size)
         self.size_slider_label.configure(text=self.brush_size)
+
+    def save_in_gallery(self):
+        # позиції канви
+        x0 = self.canvas.winfo_rootx()
+        y0 = self.canvas.winfo_rooty()
+        x1 = x0 + self.canvas.winfo_width()
+        y1 = y0 + self.canvas.winfo_height()
+
+        # вміст канви
+        canvas_img = ImageGrab.grab(bbox=(x0, y0, x1, y1))
+
+        unique_filename = f"gallery/{uuid.uuid4()}.png"
+
+        saved = CTkMessagebox(title="Збережено",
+                              message='Малюнок успішно збережено в галерею ("Моя галерея" в меню вгорі)!',
+                              icon="icons/saved.png", icon_size=(100,100))
+
+        canvas_img.save(unique_filename)
 
     def change_color(self, new_color):
         self.color = new_color
